0c438a814814d75c6d9358e34955a503115b2a41
DasAdErt <78647864+DasAdErt@users.noreply.github.com>
2023-11-16 19:02:05 +0300

    Add files via upload

---
+++ Notebook/src/main/java/com/example/notebook/NotebookController.java
@@ -0,0 +1,35 @@
+package com.example.notebook;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class NotebookController {
+    @FXML
+    Button buttonAdd;
+    @FXML
+    Button buttonDelete;
+    @FXML
+    TextField inputField;
+    @FXML
+    ListView <String> listView;
+    @FXML
+    ObservableList<String> list = FXCollections.observableArrayList();
+
+    @FXML
+    public void buttonClickAdd(){
+        String inputWords = inputField.getText();
+        list.add(inputWords);
+        listView.setItems(list);
+    }
+
+    @FXML
+    public void buttonClickDelete(){
+        String inputWordsForDelete = inputField.getText();
+        list.remove(inputWordsForDelete);
+        listView.setItems(list);
+    }
+}
